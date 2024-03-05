d7c14aa5d91e2435ecaf644486784110d92efb0c
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-03-05 17:40:06 -0300

    Feat: add next auth provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+
 export default function Home() {
-   return <div> teste</div>;
+   const { data } = useSession();
+
+   const handlerLoginClick = () => {
+      return signIn("google");
+   };
+
+   const handlerLogoutClick = () => {
+      return signOut();
+   };
+
+   return <div className=""></div>;
 }
