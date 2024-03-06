d69281050f87eeae9a9355a9ea44f80f482edb09
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-03-06 18:00:40 -0300

    Feat: add tailwind colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,30 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+   content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+   ],
+   theme: {
+      extend: {
+         backgroundImage: {
+            "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+            "gradient-conic":
+               "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+         },
+         colors: {
+            primary: "#590BD8",
+            primaryLighter: "#DDD5EA",
+            secondary: "#312A4F",
+            grayPrimary: "#717171",
+            grayLighter: "#BBBFBF",
+         },
+         textColor: {
+            dark: "#717171",
+         },
       },
-    },
-  },
-  plugins: [],
+   },
+   plugins: [],
 };
 export default config;
