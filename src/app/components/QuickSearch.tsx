ff00d03e52267ed80835185f5eca15ed50cca7c4
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-03-06 18:45:41 -0300

    Feat: add quick search component

---
+++ src/app/components/QuickSearch.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+
+const QuickSearch = () => {
+   return (
+      <div className="container mx-auto px-5 py-2">
+         <div className="flex items-center">
+            <div className="w-full h-[2px] bg-primaryLighter"></div>
+            <h2 className="text-grayPrimary font-medium whitespace-nowrap px-5">
+               Tente pesquisar por
+            </h2>
+            <div className="w-full h-[2px] bg-primaryLighter"></div>
+         </div>
+         <div className="flex justify-between mt-5">
+            <div className="flex flex-col items-center gap-1">
+               <Image
+                  width={30}
+                  height={30}
+                  src="/hotel-icon.png"
+                  alt="hotels"
+               />
+               <p className="text-sm text-grayPrimary">Hotel</p>
+            </div>
+
+            <div className="flex flex-col items-center gap-1">
+               <Image
+                  width={30}
+                  height={30}
+                  src="/farm-icon.png"
+                  alt="Fazendas"
+               />
+               <p className="text-sm text-grayPrimary">Fazenda</p>
+            </div>
+
+            <div className="flex flex-col items-center gap-1">
+               <Image
+                  width={30}
+                  height={30}
+                  src="/cottage-icon.png"
+                  alt="Chales"
+               />
+               <p className="text-sm text-grayPrimary">Chalé</p>
+            </div>
+
+            <div className="flex flex-col items-center gap-1">
+               <Image
+                  width={30}
+                  height={30}
+                  src="/inn-icon.png"
+                  alt="pouzadas"
+               />
+               <p className="text-sm text-grayPrimary">Pouzada</p>
+            </div>
+         </div>
+      </div>
+   );
+};
+
+export default QuickSearch;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import QuickSearch from "./components/QuickSearch";
 import Search from "./components/TripInput/search";
 
 export default function Home() {
    return (
       <>
          <Search />
+         <QuickSearch />
       </>
    );
 }
