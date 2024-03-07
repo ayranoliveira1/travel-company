916cd12641be88567e88cd1602834675757bd19c
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-03-07 15:20:23 -0300

    Feat: add trip reservation component

---
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import Button from "@/app/components/TripInput/components/Button";
+import DatePicker from "@/app/components/TripInput/components/DatePicker";
+import Input from "@/app/components/TripInput/components/input";
+import { Trip } from "@prisma/client";
+
+interface TripReservationProps {
+   trip: Trip;
+}
+
+const TripReservation = ({ trip }: TripReservationProps) => {
+   return (
+      <div className="flex flex-col px-5 gap-3">
+         <div className="flex gap-4">
+            <DatePicker
+               placeholderText="Data de início"
+               onChange={() => {}}
+               className="w-full"
+            />
+            <DatePicker
+               placeholderText="Data final"
+               onChange={() => {}}
+               className="w-full"
+            />
+         </div>
+         <Input placeholder={`Hóspedes (max: ${trip.maxGuests})`} />
+         <div className="flex justify-between mt-1">
+            <p className="text-sm font-medium text-secondary">
+               Total (7 noites)
+            </p>
+            <p className="text-sm font-medium text-secondary">R$2.660</p>
+         </div>
+         <Button>Reservar agora</Button>
+      </div>
+   );
+};
+
+export default TripReservation;
--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -1,5 +1,6 @@
 import { db } from "@/lib/prisma";
 import TripHeader from "./components/TripHeader";
+import TripReservation from "./components/TripReservation";
 
 const TripDetails = async ({ params }: { params: { tripId: string } }) => {
    const trip = await db.trip.findUnique({
@@ -15,6 +16,8 @@ const TripDetails = async ({ params }: { params: { tripId: string } }) => {
          <TripHeader trip={trip} />
 
          {/* Reserva */}
+
+         <TripReservation trip={trip} />
       </div>
    );
 };
