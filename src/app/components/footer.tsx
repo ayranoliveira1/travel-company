43bcb029123df5306b9c7458052ff8e7531bb6e1
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-03-15 11:01:04 -0300

    Style: fixing the footer at the end of the page

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Footer = () => {
    return (
-      <footer className="bg-walterWhite p-5 flex flex-col items-center gap-1">
+      <footer className="bg-walterWhite p-5 fixed bottom-0 w-full flex flex-col items-center gap-1">
          <Image src="/logo.png" alt="Travel Company" width={133} height={33} />
          <p className="text-sm font-medium text-secondary">
             Todos os direitos reservados.
