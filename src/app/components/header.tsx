dcba31a12980109c831b7d4f6a400f5dabd07ac4
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-03-27 10:27:04 -0300

    Style: add resposiveness in the header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -21,7 +21,7 @@ const Header = () => {
    };
 
    return (
-      <header className="container mx-auto p-5 py-0 h-[93px] lg:px-24 flex justify-between items-center">
+      <header className="container mx-auto p-5 py-0 h-[93px] lg:px-24 flex justify-between items-center lg:border-b lg:border-solid lg:border-gray-250">
          <div className="relative w-[183px] h-[32px]">
             <Link href="/#">
                <Image src="/logo.png" alt="Logo" fill />
