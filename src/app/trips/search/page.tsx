aa39b71540604dcf28a5d15a13fd4208bd7dbe1f
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-03-27 10:33:05 -0300

    Style: add responsiveness to trip search

--- src/app/trips/search/page.tsx
+++ src/app/trips/search/page.tsx
@@ -29,8 +29,8 @@ const Trips = () => {
    }, [searchParams]);
 
    return (
-      <div className="container flex flex-col mx-auto items-center p-5">
-         <h1 className="text-xl font-semibold text-secondary">
+      <div className="container flex flex-col mx-auto items-center p-5 lg:items-start lg:px-24 lg:gap-8">
+         <h1 className="text-xl font-semibold text-secondary lg:text-3xl">
             Hospedagens encontradas
          </h1>
          <h2 className=" font-medium text-grayPrimary mb-5">
@@ -39,7 +39,7 @@ const Trips = () => {
                : "Não encontramos nenhuma viagem"}
          </h2>
 
-         <div className="flex flex-col gap-4">
+         <div className="flex flex-col gap-4 lg:flex-row lg:gap-12 lg:flex-wrap">
             {trips.map((trip) => (
                <TripItem key={trip.id} trip={trip} />
             ))}
