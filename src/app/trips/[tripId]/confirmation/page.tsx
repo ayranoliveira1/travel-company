adcaf713545743eb6f3b1a264cfc995f04ec82b7
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-04-10 11:09:27 -0300

    Fix: add redirection to /reservation before fixing stripe error

--- src/app/trips/[tripId]/confirmation/page.tsx
+++ src/app/trips/[tripId]/confirmation/page.tsx
@@ -10,7 +10,6 @@ import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import ReactCountryFlag from "react-country-flag";
 import { toast } from "react-toastify";
-import { loadStripe } from "@stripe/stripe-js";
 
 const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
    const [trip, setTrip] = useState<Trip | null>();
@@ -53,18 +52,16 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
    if (!trip) return null;
 
    const handleBuyClick = async () => {
-      const response = await fetch("/api/payment", {
+      const response = await fetch("/api/trip/reservation", {
          method: "POST",
          body: Buffer.from(
             JSON.stringify({
                tripId: params.tripId,
-               totalPrice,
+               totalPaid: totalPrice,
                startDate: searchParams.get("startDate"),
                endDate: searchParams.get("endDate"),
                guests: Number(searchParams.get("guests")),
-               coverImage: trip.coverImage,
-               name: trip.name,
-               description: trip.description,
+               userId: (data?.user as any)?.id,
             })
          ),
       });
@@ -75,17 +72,11 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
          });
       }
 
-      const { sessionId } = await response.json();
-
-      const stripe = await loadStripe(
-         process.env.NEXT_PUBLIC_STRIPE_KEY as string
-      );
-
-      await stripe?.redirectToCheckout({ sessionId });
-
       toast.success("Reserva efetuada com sucesso", {
          position: "bottom-center",
       });
+
+      router.push("/");
    };
 
    const startDate = new Date(searchParams.get("startDate") as string);
